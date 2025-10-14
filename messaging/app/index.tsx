d568367bf0291168eb107c0076e2ea33a206e333
a-cuc <148326642+a-cuc@users.noreply.github.com>
2025-10-14 09:32:45 +0000

    added part 1 of procedure

--- messaging/app/index.tsx
+++ messaging/app/index.tsx
@@ -1,15 +1,30 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <View style={styles.container}>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container : {
+    flex : 1,
+    backgroundColor : 'white'
+  },
+  content : {
+    flex : 1,
+    backgroundColor : 'white'
+  },
+  inputMethodEditor : {
+    flex : 1,
+    backgroundColor : 'white'
+  },
+  toolbar : {
+    borderTopWidth : 1,
+    borderTopColor : 'rgba(0,0,0,0.04)'
+  }
+})
