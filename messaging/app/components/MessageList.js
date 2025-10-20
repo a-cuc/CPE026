cb7b959303f550afd0434009007feb9632c78189
a-cuc <148326642+a-cuc@users.noreply.github.com>
2025-10-20 08:18:05 +0000

    added procedure for HOA 10.1

---
+++ messaging/app/components/MessageList.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { MessageShape } from '../utils/MessageUtils';
+
+const keyExtractor = (item) => item.id.toString();
+
+export default class MessageList extends React.PureComponent {
+    static propTypes = {
+        messages: PropTypes.arrayOf(MessageShape).isRequired,
+        onPressMessage: PropTypes.func,
+    };
+    static defaultProps = {
+        onPressMessage: () => {},
+    };
+    renderMessageItem = ({ item }) => {
+        const { onPressMessage } = this.props;
+        return (
+            <View key={item.id} style={styles.messageRow}>
+                <TouchableOpacity onPress={() => onPressMessage(item)}>
+                    {this.renderMessageBody(item)}
+                </TouchableOpacity>
+            </View>
+        );
+    };
+    renderMessageBody({ type, text, uri, coordinate }) {
+        switch (type) {
+            case 'text':
+                return (
+                    <View style={styles.messageBubble}>
+                        <Text style={styles.text}>{text}</Text>
+                    </View>
+                );
+            case 'image':
+                return (
+                    <Image
+                        source={{ uri }}
+                        style={styles.image}
+                    />
+                );
+            case 'location':
+                return (
+                    <MapView
+                        style={styles.map}
+                        initialRegion={{
+                            ...coordinate,
+                            latitudeDelta: 0.08,
+                            longitudeDelta: 0.04,
+                        }}
+                    >
+                        <Marker coordinate={coordinate} />
+                    </MapView>
+                );
+            default:
+                return null;
+        }
+    }
+
+    render() {
+        const { messages } = this.props;
+        return (
+            <FlatList
+                style={styles.container}
+                inverted
+                data={messages}
+                renderItem={this.renderMessageItem}
+                keyExtractor={keyExtractor}
+                keyboardShouldPersistTaps={"handled"}
+            />
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        overflow: 'visible',
+    },
+    messageRow: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        marginLeft: 60,
+    },
+    messageBubble: {
+        backgroundColor: '#007aff',
+        borderRadius: 16,
+        padding: 10,
+        marginVertical: 4,
+        maxWidth: '80%',
+        color: 'white',
+    },
+    text: {
+        color: 'white',
+        fontSize: 16,
+    },
+    image: {
+        width: 150,
+        height: 100,
+        borderRadius: 10,
+        marginVertical: 4,
+    },
+    map: {
+        width: 150,
+        height: 100,
+        borderRadius: 10,
+        marginVertical: 4,
+    },
+});
--- messaging/app/index.tsx
+++ messaging/app/index.tsx
@@ -1,10 +1,23 @@
 import { Text, View, StyleSheet } from "react-native";
 import Status from "./components/Status";
+import React from "react";
+import MessageList from "./components/MessageList";
+import { createImageMessage, createLocationMessage, createTextMessage } from "./utils/MessageUtils";
+
+const state = {
+  messages: [
+    createImageMessage('https://unsplash.it/300/300'),
+    createTextMessage('World      '),
+    createTextMessage('Hello      '),
+    createLocationMessage({ latitude: 37.78825, longitude: -122.4324 }),
+  ],
+};
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Status />
+      <MessageList messages={state.messages} />
       <View style={styles.content}>
         
       </View>
